87d09dce450c8c6e5c7ca5e2173a8ee869751aa5
romain.wg <romain.wg@live.fr>
2023-10-29 20:35:19 +0100

    Update compile_module.sh

--- compile_module.sh
+++ compile_module.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+
 #Test linux distribution and version
 source /etc/os-release
 if [ "$ID" != "ubuntu" ]; then 
@@ -11,23 +12,27 @@ if [ "$VERSION_ID" != "22.04" ]; then
 	exit
 fi
 
-
 sudo apt update # update package list
 sudp apt upgrade # upgrade packages
-sudo apt install build-essential -y # install tools needed for module compilation
+sudo apt install build-essential cheese -y # install tools needed for module compilation & cheese for test webcam
 
 #get driver code to compile a patch it
 cd ~  # change to your home directory
-apt-get source linux-modules-extra-$(uname -r)  #download in your home, the kernel source file version that match your used kernel
+
+# from https://github.com/Giuliano69/uvc_driver-for-Quanta-HD-User-Facing-0x0408-0x4035-/issues/3#issuecomment-1715009720
+wget http://archive.ubuntu.com/ubuntu/pool/main/l/linux-hwe-6.2/linux-hwe-6.2_6.2.0.orig.tar.gz && \
+	tar -xzf linux-hwe-6.2_6.2.0.orig.tar.gz
 cd ~/linux-*/drivers/media/usb/uvc # change to the currently created uvc directory
 mv uvc_driver.c uvc_driver.old  # rename/backup the uvc driver soruce file, that need to be updated
 wget https://raw.githubusercontent.com/Giuliano69/uvc_driver-for-Quanta-HD-User-Facing-0x0408-0x4035-/main/uvc_driver.c # download the updated driver source file
 
 #compile and install
 make -j4 -C /lib/modules/$(uname -r)/build M=$(pwd) modules  # complie the updated video modules for your kernel version
-sudo cp uvcvideo.ko /lib/modules/$(uname -r)/kernel/drivers/media/usb/uvc/  #install the video driver module in the system
+sudo cp /lib/modules/$(uname -r)/kernel/drivers/media/usb/uvc/uvcvideo.ko ~/linux-*/drivers/media/usb/uvc/uvcvideo.ko.backup # backup
+sudo cp uvcvideo.ko /lib/modules/$(uname -r)/kernel/drivers/media/usb/uvc/ # install the video driver module in the system (overwrite the previous one)
+
+# Remove and add uvcvideo module
 sudo rmmod uvcvideo && sudo modprobe uvcvideo
-#reboot  #reboot to check your camera is working
 
-
-
+# check if it is ok - no need to reboot for me :)
+cheese
